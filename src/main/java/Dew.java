6709b2021bf360f383fc88c9e0ba6ccda9eb53cc
panomia01 <72587556+panomia01@users.noreply.github.com>
2025-01-24 04:30:11 +0800

    added different tier of tasks

--- src/main/java/Dew.java
+++ src/main/java/Dew.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Dew {
@@ -21,30 +25,86 @@ public class Dew {
         String line = "____________________________________________________________\n";
         System.out.println(start_dialogue);
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] items = new String[100];
+        String input = scanner.next();
+//        String[] userInput = input.split(" ");
+        Task[] items = new Task[100];
         int counter = 0;
         while (!input.equals("bye")) {
+
             if (input.equals("list")) {
-                System.out.println("____________________________________________________________");
+                System.out.println("____________________________________________________________\n"
+                    + "Here are the tasks in your list:");
                 int itemSize = items.length;
                 for (int i = 0; i < itemSize; i++) {
                     int itemIndex = i + 1;
                     if (items[i] == null) {
                         break;
                     }
-                    System.out.println(itemIndex + ". " + items[i]);
+                    System.out.println(itemIndex + "." + items[i]);
                 }
                 System.out.println(line);
-            } else {
-                System.out.println(line
-                        + "added: " + input + "\n"
+            } else if (input.equals("unmark")) {
+                String num = scanner.next();
+                int itemIndex = Integer.parseInt(num) - 1;
+                items[itemIndex].unMarkStatusIcon();
+
+                System.out.println("____________________________________________________________\n"
+                        + "OK, I've marked this task as not done yet:\n"
+                        + "   " + items[itemIndex] + "\n"
                         + line);
-                items[counter] = input;
-                counter++;
+            } else if (input.equals("mark")) {
+                String num = scanner.next();
+                int itemIndex = Integer.parseInt(num) - 1;
+                items[itemIndex].markStatusIcon();
+
+                System.out.println("____________________________________________________________\n"
+                        + "Nice! I've marked this task as done:\n"
+                        + "   " + items[itemIndex] + "\n"
+                        + line);
+            } else if (input.equals("todo")) {
+                String desc = scanner.nextLine();
+                items[counter] = new Todo(desc);
+
+                System.out.println("____________________________________________________________\n"
+                        + "Got it. I've added this task:\n"
+                        + "   " + items[counter] + "\n"
+                        + "Now you have " + ++counter +" tasks in the list." + "\n"
+                        + line);
+            } else if (input.equals("deadline")) {
+                String nextInput = scanner.nextLine();
+                String[] userInput = nextInput.split(" /by ");
+                items[counter] = new Deadline(userInput[0], userInput[1]);
+
+                System.out.println("____________________________________________________________\n"
+                        + "Got it. I've added this task:\n"
+                        + "   " + items[counter] + "\n"
+                        + "Now you have " + ++counter +" tasks in the list." + "\n"
+                        + line);
+            } else if (input.equals("event")) {
+                String nextInput = scanner.nextLine();
+                String[] userInput = nextInput.split(" /");
+                userInput[1] = userInput[1].substring(5);
+                userInput[2] = userInput[2].substring(3);
+                items[counter] = new Event(userInput[0], userInput[1], userInput[2]);
+
+                System.out.println("____________________________________________________________\n"
+                        + "Got it. I've added this task:\n"
+                        + "   " + items[counter] + "\n"
+                        + "Now you have " + ++counter +" tasks in the list." + "\n"
+                        + line);
             }
-            input = scanner.nextLine();
+//            else {
+//                System.out.println(line
+//                        + "added: " + input + "\n"
+//                        + line);
+//                items[counter] = new Task(input);
+//                counter++;
+//            }
+            input = scanner.next();
+//            userInput = input.split(" ");
         }
         System.out.println(end_dialogue);
     }
+
+//
 }
---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends  Task{
+
+    protected String timeStart;
+    protected String timeEnd;
+
+    public Event(String description, String timeStart, String timeEnd) {
+        super(description);
+        this.timeEnd = timeEnd;
+        this.timeStart = timeStart;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + timeStart + " to: " + timeEnd + ")";
+    }
+}
