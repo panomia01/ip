9f6ed2bf52583508eb32d6539f1810fee38089b2
panomia01 <72587556+panomia01@users.noreply.github.com>
2025-01-24 14:33:47 +0800

    level 5 and 6 completed

--- src/main/java/Dew.java
+++ src/main/java/Dew.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,79 +28,105 @@ public class Dew {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 //        String[] userInput = input.split(" ");
-        Task[] items = new Task[100];
+//        Task[] items = new Task[100];
+        ArrayList<Task> items = new ArrayList<>();
         int counter = 0;
         while (!input.equals("bye")) {
-
-            if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n"
-                    + "Here are the tasks in your list:");
-                int itemSize = items.length;
-                for (int i = 0; i < itemSize; i++) {
-                    int itemIndex = i + 1;
-                    if (items[i] == null) {
-                        break;
+            try {
+                if (input.equals("list")) {
+                    System.out.println("____________________________________________________________\n"
+                            + "Here are the tasks in your list:");
+                    int itemSize = items.size();
+                    for (int i = 0; i < itemSize; i++) {
+                        int itemIndex = i + 1;
+                        if (items.get(i) == null) {
+                            break;
+                        }
+                        System.out.println(itemIndex + "." + items.get(i));
                     }
-                    System.out.println(itemIndex + "." + items[i]);
+                    System.out.println(line);
+                } else if (input.equals("unmark")) {
+                    String num = scanner.next();
+                    int itemIndex = Integer.parseInt(num) - 1;
+                    items.get(itemIndex).unMarkStatusIcon();
+
+                    System.out.println("____________________________________________________________\n"
+                            + "OK, I've marked this task as not done yet:\n"
+                            + "   " + items.get(itemIndex) + "\n"
+                            + line);
+                } else if (input.equals("mark")) {
+                    String num = scanner.next();
+                    int itemIndex = Integer.parseInt(num) - 1;
+                    items.get(itemIndex).markStatusIcon();
+
+                    System.out.println("____________________________________________________________\n"
+                            + "Nice! I've marked this task as done:\n"
+                            + "   " + items.get(itemIndex) + "\n"
+                            + line);
+                } else if (input.equals("todo")) {
+                    String desc = scanner.nextLine();
+                    if (desc.isEmpty()) {
+                        throw new DewException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+//                    items.get(counter) = new Todo(desc);
+                    items.add(new Todo(desc));
+
+                    System.out.println("____________________________________________________________\n"
+                            + "Got it. I've added this task:\n"
+                            + "   " + items.get(counter) + "\n"
+                            + "Now you have " + ++counter +" tasks in the list." + "\n"
+                            + line);
+                } else if (input.equals("deadline")) {
+                    String nextInput = scanner.nextLine();
+                    String[] userInput = nextInput.split(" /by ");
+                    if (userInput.length != 2) {
+                        throw new DewException("OOPS!!! The description of a Deadline is not fully filled");
+                    }
+//                    items[counter] = new Deadline(userInput[0], userInput[1]);
+                    items.add(new Deadline(userInput[0], userInput[1]));
+
+                    System.out.println("____________________________________________________________\n"
+                            + "Got it. I've added this task:\n"
+                            + "   " + items.get(counter) + "\n"
+                            + "Now you have " + ++counter +" tasks in the list." + "\n"
+                            + line);
+                } else if (input.equals("event")) {
+                    String nextInput = scanner.nextLine();
+                    String[] userInput = nextInput.split(" /");
+                    if (userInput.length != 3) {
+                        throw new DewException("OOPS!!! The description of a Event is not fully filled");
+                    }
+                    userInput[1] = userInput[1].substring(5);
+                    userInput[2] = userInput[2].substring(3);
+//                    items[counter] = new Event(userInput[0], userInput[1], userInput[2]);
+                    items.add(new Event(userInput[0], userInput[1], userInput[2]));
+
+                    System.out.println("____________________________________________________________\n"
+                            + "Got it. I've added this task:\n"
+                            + "   " + items.get(counter) + "\n"
+                            + "Now you have " + ++counter +" tasks in the list." + "\n"
+                            + line);
+                } else if (input.equals("delete")) {
+                    String nextInput = scanner.next();
+                    int itemIndex = Integer.parseInt(nextInput) - 1;
+//                    String[] userInput = nextInput.split(" /");
+                    if (items.get(itemIndex) == null) {
+                        throw new DewException("OOPS!!! The item in delete is not valid");
+                    }
+
+                    System.out.println("____________________________________________________________\n"
+                            + "Noted. I've removed this task:\n"
+                            + "   " + items.get(itemIndex) + "\n"
+                            + "Now you have " + --counter +" tasks in the list." + "\n"
+                            + line);
+                    items.remove(itemIndex);
+                } else {
+                    throw new DewException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                System.out.println(line);
-            } else if (input.equals("unmark")) {
-                String num = scanner.next();
-                int itemIndex = Integer.parseInt(num) - 1;
-                items[itemIndex].unMarkStatusIcon();
-
-                System.out.println("____________________________________________________________\n"
-                        + "OK, I've marked this task as not done yet:\n"
-                        + "   " + items[itemIndex] + "\n"
-                        + line);
-            } else if (input.equals("mark")) {
-                String num = scanner.next();
-                int itemIndex = Integer.parseInt(num) - 1;
-                items[itemIndex].markStatusIcon();
-
-                System.out.println("____________________________________________________________\n"
-                        + "Nice! I've marked this task as done:\n"
-                        + "   " + items[itemIndex] + "\n"
-                        + line);
-            } else if (input.equals("todo")) {
-                String desc = scanner.nextLine();
-                items[counter] = new Todo(desc);
-
-                System.out.println("____________________________________________________________\n"
-                        + "Got it. I've added this task:\n"
-                        + "   " + items[counter] + "\n"
-                        + "Now you have " + ++counter +" tasks in the list." + "\n"
-                        + line);
-            } else if (input.equals("deadline")) {
-                String nextInput = scanner.nextLine();
-                String[] userInput = nextInput.split(" /by ");
-                items[counter] = new Deadline(userInput[0], userInput[1]);
-
-                System.out.println("____________________________________________________________\n"
-                        + "Got it. I've added this task:\n"
-                        + "   " + items[counter] + "\n"
-                        + "Now you have " + ++counter +" tasks in the list." + "\n"
-                        + line);
-            } else if (input.equals("event")) {
-                String nextInput = scanner.nextLine();
-                String[] userInput = nextInput.split(" /");
-                userInput[1] = userInput[1].substring(5);
-                userInput[2] = userInput[2].substring(3);
-                items[counter] = new Event(userInput[0], userInput[1], userInput[2]);
-
-                System.out.println("____________________________________________________________\n"
-                        + "Got it. I've added this task:\n"
-                        + "   " + items[counter] + "\n"
-                        + "Now you have " + ++counter +" tasks in the list." + "\n"
-                        + line);
+            } catch (DewException e) {
+                System.out.println(e.getMessage());
             }
-//            else {
-//                System.out.println(line
-//                        + "added: " + input + "\n"
-//                        + line);
-//                items[counter] = new Task(input);
-//                counter++;
-//            }
+
             input = scanner.next();
 //            userInput = input.split(" ");
         }
---
+++ src/main/java/DewException.java
@@ -0,0 +1,7 @@
+public class DewException extends Exception {
+    public DewException(String message) {
+        super("____________________________________________________________\n"
+                + message + "\n"
+        + "_________________________________________________________________\n");
+    }
+}
